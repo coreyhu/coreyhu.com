e84766412b4cb41377c4447a039892abccf0c29d
Corey Hu <corey.huym@gmail.com>
2020-04-23 01:26:07 -0700

    finish homepage (minus content)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 function menuToggle(button, menu) {
-    button.classList.toggle("button-active");
+    button.classList.toggle("hamburger-button-active");
 
     button.childNodes[1].classList.toggle("drop-shadow");
     button.childNodes[3].classList.toggle("drop-shadow");
@@ -8,15 +8,6 @@ function menuToggle(button, menu) {
     menu.classList.toggle("menu-active");
 }
 
-// function copyAddress() {
-//     var copyText = document.getElementById("myInput");
-//     copyText.select();
-//     copyText.setSelectionRange(0, 99999);      
-//     document.execCommand("copy");
-      
-// }
-
 var menuButton = document.getElementById('hamburger-icon');
 var hamburgerMenu = document.getElementById('hamburger-menu');
 menuButton.addEventListener('click', function() { menuToggle(menuButton, hamburgerMenu); });
-
